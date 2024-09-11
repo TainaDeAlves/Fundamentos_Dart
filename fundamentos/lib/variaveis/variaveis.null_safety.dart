79ecc513b9e14436b11d48b40e0e6657736a49f5
Tainá Souza <tainadesouzalves@gmail.com>
2024-09-10 21:59:32 -0300

    copiladores

--- fundamentos/lib/variaveis/variaveis.null_safety.dart
+++ fundamentos/lib/variaveis/variaveis.null_safety.dart
@@ -1,13 +1,11 @@
-void main(){
-    // null safety é uma caracteristica introduzida no dart a partir da versão 2.12 que visa erros em tempo de execução. O null safety torna o sistema  de tipagem forte, mais rigorosa 
+void main() {
+  // null safety é uma caracteristica introduzida no dart a partir da versão 2.12 que visa erros em tempo de execução. O null safety torna o sistema  de tipagem forte, mais rigorosa
 
-    // variaveis non-nullable
+  // variaveis non-nullable
 
-    
+  String nomecompleto;
+  nomecompleto = "fundamento dart";
 
-    String nomecompleto;
-    nomecompleto = "fundamento dart";
-
-     String nomecompletoatribuicao = null;
-    nomecompleto = "fundamento dart";
+ // String nomecompletoatribuicao = null;
+  nomecompleto = "fundamento dart";
 }
